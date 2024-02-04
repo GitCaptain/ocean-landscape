6e61dc801dbb4f6ff5ded8d73b0fc0d37546e672
Alexander Sukhikh <alexsm.rf24@gmail.com>
2024-02-04 20:35:25 +0300

    Implement simple Voronoi diagram algorithm

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,4 +1,6 @@
 #include <random>
+#include <cstdlib>
+#include <algorithm>
 #include "generator.h"
 #include "logger.h"
 
@@ -8,6 +10,7 @@ void Generator::generate() {
     LOG(std::cout << "Generation process started\n";);
     setup_map();
     split_map();
+    set_height();
     set_properties();
     simulate();
 }
@@ -20,33 +23,40 @@ void Generator::setup_map() {
     LOG(std::cout << "Setting up map...\n";);
     map.resize(sizex, std::vector<Voxel>(sizey, {1, -1, 0}));
     long long space = sizex * 1ll *sizey;
-    int plates_count = 5; // get_random_number_in_range(space / 200, space / 100);
+    int plates_count = get_random_number_in_range(5, 15);
     plates.resize(plates_count);
 }
 
 void Generator::split_map() {
-    // Replace this with something like Voronoi diagram
-    LOG(std::cout << "Split map...\n";);
-    for(int i = 0; i < sizex; i++) {
-        for(int j = 0; j < sizey; j++) {
-            if (i > sizex / 4 &&
-                i < sizex / 4 * 3 &&
-                j > sizey / 4 &&
-                j < sizey / 4 * 3) {
-                    map[i][j].plate_ref = 4;
+    LOG(std::cout << "Split map into " << plates.size() << " plates...\n";);
+
+    struct Point {
+        int x;
+        int y;
+    };
+
+    std::vector<Point> points(plates.size());
+    // generate random points on plate to calcalute
+    // Voronoi diagram depending on them.
+    for(auto &p: points) {
+        p.x = get_random_number_in_range(0, map.size());
+        p.y = get_random_number_in_range(0, map[0].size());
+    }
+
+    // Discrete voronoi diagram
+    for(int x = 0; x < sizex; x++) {
+        for(int y = 0; y < sizey; y++) {
+            int min_dist = -1;
+            int point_index = 0;
+            for (int i = 0; i < points.size(); i++) {
+                Point &p = points[i];
+                int cur_dist = std::abs(x - p.x) + std::abs(y - p.y);
+                if (min_dist < 0 || cur_dist < min_dist) {
+                    min_dist = cur_dist;
+                    point_index = i;
                 }
-            else if (i < sizex / 2 && j < sizex / 2) {
-                map[i][j].plate_ref = 0;
             }
-            else if (i >= sizex / 2 && j < sizex / 2) {
-                map[i][j].plate_ref = 1;
-            }
-            else if (i >= sizex / 2 && j >= sizex / 2) {
-                map[i][j].plate_ref = 2;
-            }
-            else if (i < sizex / 2 && j >= sizex / 2) {
-                map[i][j].plate_ref = 3;
-            }
+            map[x][y].plate_ref = point_index;
         }
     }
 }
@@ -64,7 +74,9 @@ void Generator::set_height() {
     LOG(std::cout << "Set heights...\n";);
     for(int i = 0; i < sizex; i++) {
         for(int j = 0; j < sizey; j++) {
-            map[i][j].height = 1;
+            map[i][j].height = 10;
+            // Palette has only 256 colors
+            map[i][j].color = map[i][j].plate_ref % 256;
         }
     }
 }
